de05ce5b92a4481ec09144d02633b455f955dc13
Maram <maramahmadbakar@gmail.com>
2022-01-25 23:41:58 +0400

    init repo

---
+++ Wahat_ALShifaTests/Wahat_ALShifaTests.swift
@@ -0,0 +1,33 @@
+//
+//  Wahat_ALShifaTests.swift
+//  Wahat_ALShifaTests
+//
+//  Created by Maram Bakar on 17/03/2021.
+//
+
+import XCTest
+@testable import Wahat_ALShifa
+
+class Wahat_ALShifaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
